7987af6c2e3041d0017b185c3233c7f871686ebe
lenovo <ricardo.roldanp@gmail.com>
2023-09-14 23:45:13 +0200

    ForoAlura V0

---
+++ src/main/java/com/alura/foro/SistemaDeForoApplication.java
@@ -0,0 +1,13 @@
+package com.alura.foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeForoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/alura/foro/view/Demo.java
@@ -0,0 +1,12 @@
+package com.alura.foro.view;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Prueba");
+		System.out.println("Prueba1");
+	}
+
+}
